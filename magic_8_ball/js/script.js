17f03fba5abb6abe553cce03ac6d814e10d2a0e2
belliocheerio <aknowlton28@eastsideprep.org>
2024-05-22 14:02:44 -0700

    Add files via upload

---
+++ magic_8_ball/js/script.js
@@ -0,0 +1,33 @@
+//alert("javascript works")
+function magic8ball() {
+    askAQuestion();
+    shake();
+    getAFortune();
+}
+
+function askAQuestion (){
+    //this does nothing
+    //alert("Ask a yes/no question and recieve")
+}
+function shake () {
+    //alert("I'm shaking")
+}
+function getAFortune() {
+    //random
+    //to choose an answer
+    //list of answers
+    var fortunes = ["yes","no","maybe"]
+    var num = randomNumber(fortunes.length)
+    displayFortune(fortunes[num])
+}
+
+function displayFortune(fortune){
+    //alert(fortune)
+    document
+    .getElementById("fortune")
+    .innerHTML = fortune
+}
+
+function randomNumber(n) {
+    return Math.floor(Math.random() * n)
+}
